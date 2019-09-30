3c4553cc7d69215ce26bfadeca4ecf3f367ec836
Shardul <shardul@ualberta.ca>
2019-09-30 13:28:25 -0600

    Super brute force, inefficient solution to p7

---
+++ p7.py
@@ -0,0 +1,17 @@
+"""
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+"""
+
+def main():
+	i = 2520
+	while True:
+		if (i%1 == 0 and i%2 == 0 and i%3 == 0 and i%4 == 0 and i%5 == 0 and i%6 == 0 and i%7 == 0 and i%8 == 0 and i%9 == 0 and i%10 == 0 and i%11 == 0 and i%12 == 0 and i%13 == 0 and i%14 == 0 and i%15 == 0 and i%16 == 0 and i%17 == 0 and i%18 == 0 and i%19 == 0 and i%20 == 0):
+			break 
+		i+=1
+
+	print("The answer to problem 7 is: " + str(i) + ".")
+
+if __name__ == "__main__":
+	main()
